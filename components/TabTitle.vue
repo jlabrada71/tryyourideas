b6ab22a7e3f9d13a0c9cf976ba6d398812eab848
jlabrada <jlabrada@yahoo.com>
2023-02-27 16:34:36 -0300

    Adding tabs component and demo

---
+++ components/TabTitle.vue
@@ -0,0 +1,38 @@
+<template>
+  <li class="mr-2">
+    <a href="#" :class="currentClass" @click="currentClass = !props.active">
+      <slot></slot>
+    </a>
+  </li>
+</template>
+
+<script setup>
+  import { ref, computed } from 'vue'
+
+  const emit = defineEmits(['update:active'])
+  const props = defineProps({
+    active: {
+      type: Boolean,
+      default: false
+    }
+  })
+
+  const defaultClass = "inline-flex p-4 border-b-2 rounded-t-lg group border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
+  const activeClass =  "inline-flex p-4 border-b-2 rounded-t-lg group active text-blue-600 border-blue-600 dark:text-blue-500 dark:border-blue-500"
+
+  const currentClass = computed({
+    get() {
+      return props.active ? activeClass: defaultClass
+    },
+    set(value) {
+      console.log(value)
+      emit('update:active', value)
+    }
+  })
+
+  function clicked() {
+    console.log(props.active)
+    console.log(currentClass.value)
+    currentClass.value = !props.active    
+  }
+</script>

e06cb0e7e80633804644e88c0fc7d9b268cbaff9
jlabrada <jlabrada@yahoo.com>
2023-03-02 18:43:20 -0300

    Using the component renderedClass

--- components/HtmlExporter.js
+++ components/HtmlExporter.js
@@ -2,8 +2,15 @@ function childrenCode(list) {
   const result = list.map(item => toHtml(item)).join('')
   return result
 }
+
+function compoundClass(component) {
+  // const result = component.classes.map(cls => cls.renderedClass).join(' ')
+  const result = component.renderedClass
+  return result;
+}
+
 export default function toHtml(component) {
-  const result = `<${component.type} id="id-${component.id}" class="${component.renderedClass}">
+  const result = `<${component.type} id="id-${component.id}" class="${compoundClass(component)}">
      ${component.text}
      ${childrenCode(component.children)}
      </${component.type}>

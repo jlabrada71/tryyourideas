8355b5d60967e85bcfa05507026e9b1132bc1d32
jlabrada <jlabrada@yahoo.com>
2023-03-07 16:51:59 -0300

    class initializer

---
+++ components/DivClassGenerator.vue
@@ -0,0 +1,21 @@
+ <template>
+  <div>
+    <div v-for="val in props.values">
+       &lt;div class="w-8 h-8 {{modifier}}{{props.tag}}-{{val}}"&gt;{{modifier}}{{props.tag}}-{{val}} &lt;/div&gt; 
+    </div>
+  </div>
+</template>
+<script setup>
+  import { ref } from 'vue'
+
+  const props = defineProps({
+    tag:{
+      type: String
+    },
+    values: {
+      type: Array
+    }
+  })
+
+  const modifiers = ['', 'hover:', 'active:', 'focus:']
+</script>

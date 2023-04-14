4bd81b546ddb15b44616302f26b21c29b14d1bdb
jlabrada <jlabrada@yahoo.com>
2023-04-14 16:39:00 -0300

    improving class generator

--- components/DivClassGenerator.vue
+++ components/DivClassGenerator.vue
@@ -1,21 +1,35 @@
  <template>
   <div>
-    <div v-for="val in props.values">
-       &lt;div class="w-8 h-8 {{modifier}}{{props.tag}}-{{val}}"&gt;{{modifier}}{{props.tag}}-{{val}} &lt;/div&gt; 
-    </div>
+    <div v-for="mode in modes">
+      <div v-for="device in devices">
+        <div v-for="modifier in modifiers">
+          <div v-for="val in props.values">
+          &lt;div class="{{c(mode, device, modifier, val)}}"&gt;{{mode}}{{device}}{{modifier}}-{{val}} &lt;/div&gt; 
+          </div>
+        </div>
+      </div>
+  </div>
+    
   </div>
 </template>
 <script setup>
   import { ref } from 'vue'
 
   const props = defineProps({
-    tag:{
-      type: String
+    tags:{
+      type: Array
     },
     values: {
       type: Array
     }
   })
 
+  function c(mode, device, modifier, value) {
+    const cls = props.tags.reduce(( a, v ) => a + `${mode}${device}${modifier}${v}-${value} `, '' )
+    return cls
+  }
+
   const modifiers = ['', 'hover:', 'active:', 'focus:']
+  const modes = ['', 'dark:']
+  const devices = ['', 'sm:', 'md:', 'lg:', 'xl:']
 </script>

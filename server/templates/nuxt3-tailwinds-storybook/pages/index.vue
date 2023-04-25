2f6f8e44ec65f8727bcae586386e568c3150bfbb
jlabrada <jlabrada@yahoo.com>
2023-04-25 19:10:43 -0300

    Removing unnecessary components from template

--- server/templates/nuxt3-tailwinds-storybook/pages/index.vue
+++ server/templates/nuxt3-tailwinds-storybook/pages/index.vue
@@ -1,10 +1,2 @@
 <template>
-    <nav class="flex align-center gap-4 p-4">
-    <NuxtLink to="/default">
-        Default layout
-    </NuxtLink>
-    <h1 class="text-lg text-gray-500">Hellow Nuxt</h1>
-    </nav>
-    <Products />
-    <Button label="This is a test button"></Button>
 </template>

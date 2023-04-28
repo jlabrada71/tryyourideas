93622ce622f0754a8917cd0704890399af2b544c
jlabrada <jlabrada@yahoo.com>
2023-04-28 10:02:05 -0300

    Taking care of self closing tags

--- components/ItemTree.vue
+++ components/ItemTree.vue
@@ -5,7 +5,7 @@
         <span class="justify-self-start w-full">{{item.type}}</span>
         <div class="hidden group-hover:block group-focus:block">
           <div class="flex flex-row justify-self-end gap-4">            
-            <button type="button" class="text-white w-5 hover:text-white font-medium rounded-lg text-sm  " @click.stop="emit('update:add-child', item )">
+            <button v-if="!isSelfClosing" type="button" class="text-white w-5 hover:text-white font-medium rounded-lg text-sm  " @click.stop="emit('update:add-child', item )">
               <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 122.88 119.8">
                 <title>Add item</title>
                 <path d="M23.59,0h75.7a23.63,23.63,0,0,1,23.59,23.59V96.21A23.64,23.64,0,0,1,99.29,119.8H23.59a23.53,23.53,0,0,1-16.67-6.93l-.38-.42A23.49,23.49,0,0,1,0,96.21V23.59A23.63,23.63,0,0,1,23.59,0ZM55.06,38.05a6.38,6.38,0,1,1,12.76,0V53.51H83.29a6.39,6.39,0,1,1,0,12.77H67.82V81.75a6.38,6.38,0,0,1-12.76,0V66.28H39.59a6.39,6.39,0,1,1,0-12.77H55.06V38.05ZM99.29,12.77H23.59A10.86,10.86,0,0,0,12.77,23.59V96.21a10.77,10.77,0,0,0,2.9,7.37l.28.26A10.76,10.76,0,0,0,23.59,107h75.7a10.87,10.87,0,0,0,10.82-10.82V23.59A10.86,10.86,0,0,0,99.29,12.77Z"/>
@@ -32,6 +32,8 @@
   </div>
 </template>
 <script setup>
+  import { selfClosingTags } from '@/lib/tags'
+
   const emit = defineEmits(['update:add-child', 'update:remove', 'selected'])
 
   const props = defineProps({
@@ -40,6 +42,7 @@
     }
   })
 
+  const isSelfClosing = computed(() => selfClosingTags.includes(props.item.type))
   const selected = computed(() => props.item.isSelected ? 'bg-slate-400 text-white' : 'bg-slate-100 text-black' )
   const itemClass = computed(() => `flex ${selected.value} hover:bg-slate-600 hover:text-slate-200 w-full group focus:bg-slate-300`)
 </script>

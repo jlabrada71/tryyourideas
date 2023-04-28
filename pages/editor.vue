331fb65831aae18b7a6922ad612faf6833e38bdb
jlabrada <jlabrada@yahoo.com>
2023-04-28 11:41:19 -0300

    Removing vue icon

--- pages/editor.vue
+++ pages/editor.vue
@@ -2,10 +2,6 @@
   <ExportProjectForm :project="project" :store="generateNuxtTailwindsStorybook"></ExportProjectForm>
   <IssuesForm :store="saveIssues"></IssuesForm>
   <div class="w-full h-20 bg-cyan-50 shadow-xl shadow-cyan-50 z-40 flex">
-    <button type="button" class="text-white w-12 m-2" @click="generateNuxtTailwindsStorybook">
-      <!-- <style type="text/css">.st0{fill:#4DBA87;} .st1{fill:#425466;}</style> -->
-      <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 122.88 106.42" style="enable-background:new 0 0 122.88 106.42" xml:space="preserve"><g><polygon class="st0" points="75.63,0 61.44,24.58 47.25,0 0,0 61.44,106.42 122.88,0 75.63,0"/><polygon class="st1" points="75.63,0 61.44,24.58 47.25,0 24.58,0 61.44,63.85 98.3,0 75.63,0"/></g></svg>
-    </button>
     <ExportProjectFormButton></ExportProjectFormButton>
     <IssuesFormButton></IssuesFormButton>
   </div>

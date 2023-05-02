26b3cd6e3b25ff8260f3de88380a1292398c93f2
jlabrada <jlabrada@yahoo.com>
2023-05-02 17:36:46 -0300

    removing models from ignore

---
+++ server/api/v1/models/index.post.ts
@@ -0,0 +1,24 @@
+import { log, debug } from '@/lib/logger'
+import fs from 'fs'
+import path from "path"
+
+export default defineEventHandler(async (event) => {
+  console.log('model POST')
+    const body = await readBody(event)
+    const req = event.node.req
+    const content = JSON.stringify(body, null, 2)
+    //debug(content) 
+    debug(body.name)  
+    const dir = `./server/models/${body.user}`
+
+    if (!fs.existsSync(dir)){
+      fs.mkdirSync(dir, { recursive: true });
+    }
+    debug(path.resolve(dir))
+
+    fs.writeFileSync( `${dir}/${body.name}.json`, content);
+    //return store(req, body)
+    return {
+      api: 'works'
+    }
+})
---
+++ server/models/jlabrada/generation/default/components/Component-1.vue
@@ -0,0 +1,7 @@
+<template>
+    <div id="1"  class=" flex flex-row bg-blue-500 w-10 h-10 ">
+    
+  </div>
+  
+  </template>
+  

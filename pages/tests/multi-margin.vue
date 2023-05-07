25bbc165aa3e2fdeb6432b6133e08f3b5d6d93b1
jlabrada <jlabrada@yahoo.com>
2023-05-07 20:23:54 -0300

    Adding multi margin test page

---
+++ pages/tests/multi-margin.vue
@@ -0,0 +1,46 @@
+<template>
+  <SelectorsMultiMargin
+    :top="top"
+    :left="left"
+    :right="right"
+    :bottom="bottom"
+    @update:values="values => updateValues(values) "
+    >
+  </SelectorsMultiMargin>
+</template>
+<script setup>
+  const top = ref(0)
+  const left = ref(0)
+  const right = ref(0)
+  const bottom = ref(0)
+
+  function updateValues(values) {
+    alert('updating values ' + values.prop + ':' + values.value )
+    if (values.prop == 'T') {
+      top.value = values.value
+    }
+    if (values.prop == 'L') {
+      left.value = values.value
+    }
+    if (values.prop == 'R') {
+      right.value = values.value
+    }
+    if (values.prop == 'B') {
+      bottom.value = values.value
+    }
+    if (values.prop == 'X') {
+      left.value = values.value
+      right.value = values.value
+    }
+    if (values.prop == 'Y') {
+      top.value = values.value
+      bottom.value = values.value
+    }
+    if (values.prop == 'A') {
+      top.value = values.value
+      bottom.value = values.value
+      left.value = values.value
+      right.value = values.value
+    }
+  }
+</script>

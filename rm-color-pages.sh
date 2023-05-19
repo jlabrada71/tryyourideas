49f8e7758eb7160196e707800c4e903330bc859b
jlabrada <jlabrada@yahoo.com>
2023-05-19 17:14:26 -0300

    Improving dev colors

--- rm-color-pages.sh
+++ rm-color-pages.sh
@@ -1,9 +1,5 @@
 
+rm  pages/tests/class-loader-colors-all.vue
 rm  pages/tests/class-loader-colors-any.vue
 rm  pages/tests/class-loader-colors-dark-any.vue
-rm  pages/tests/class-loader-colors-dark-lg.vue
-rm  pages/tests/class-loader-colors-dark-md.vue
-rm  pages/tests/class-loader-colors-dark-sm.vue
-rm  pages/tests/class-loader-colors-dark-xl.vue
-rm  pages/tests/class-loader-colors-lg-xl.vue
 rm  pages/tests/class-loader-colors-sm-md.vue 

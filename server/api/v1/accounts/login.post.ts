b1da87fa948c03b0ad5237a70a438c1a5ff601f6
jlabrada <jlabrada@yahoo.com>
2023-06-01 15:59:20 -0300

    Adding account activated check

--- server/api/v1/accounts/login.post.ts
+++ server/api/v1/accounts/login.post.ts
@@ -3,11 +3,13 @@ import { log, debug } from '@/lib/logger'
 import { getRandomId } from '@/lib/IdGeneration'
 import { SessionRepository } from '@/lib/SessionRepository'
 import { CredentialRepository } from '@/lib/CredentialRepository'
+import { AccountRepository } from '@/lib/AccountRepository'
 import { isPasswordCorrect } from '@/lib/PasswordUtils'
 
 const config = useRuntimeConfig()
 const sessionRepository = new SessionRepository(config)
 const credentialRepository = new CredentialRepository(config)
+const accountRepository = new AccountRepository(config)
 
 function createSession(email: string) {
   const expires = new Date();
@@ -23,6 +25,23 @@ function createSession(email: string) {
 export default defineEventHandler(async (event) => {
   log('account login POST')
   const body = await readBody(event)
+  const existingAccounts = await accountRepository.select(acc => acc.email === body.email)
+  
+  if (existingAccounts.length === 0) {
+    return { 
+      result: 'error', 
+      msg: 'Account not found for ' + body.email 
+    }
+  }
+  const account = existingAccounts[0]
+
+  if (!account.active) {
+    return { 
+      result: 'error', 
+      msg: 'Account not activated '
+    }
+  }
+
   const existingCredentials = await credentialRepository.select(acc => acc.email === body.email)
 
   if (existingCredentials.length === 0) {

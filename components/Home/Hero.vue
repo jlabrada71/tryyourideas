7cfca2385c339d307f81b16e31e2f748066559fe
jlabrada <jlabrada@yahoo.com>
2023-06-06 17:59:57 -0300

    Improving hero section

--- components/Home/Hero.vue
+++ components/Home/Hero.vue
@@ -3,8 +3,8 @@
 
         <section id="id-7-1" class=" flex flex-row">
 
-            <span id="id-7-1-1" class=" flex flex-row w-auto h-auto text-8xl font-sans ">
-                What if it works?
+            <span id="id-7-1-1" class=" flex flex-row w-auto h-auto text-8xl font-sans font-bold">
+                What if they work?
 
             </span>
 

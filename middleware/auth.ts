851e8cf04a6b048a2f53e8ba7367985ac67b3518
jlabrada <jlabrada@yahoo.com>
2023-06-16 23:42:48 -0300

    Sign in bugs

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -3,26 +3,24 @@ import { debug } from '@/lib/logger.js'
 import { useStorage } from '@vueuse/core'
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
-  const config = useRuntimeConfig()
+  // const config = useRuntimeConfig()
 
-  const accessToken = useCookie('access_token')
-  const refreshToken = useCookie('refresh_token')
-  const currentUser = useStorage('user', {
-    name: 'anonimous',
-    email: 'undefined',
-    id: 'undefined',
-    licence: 'community',
-    maxProjects: '1'
-  })
+  // const currentUser = useStorage('user', {
+  //   name: 'anonimous',
+  //   email: 'undefined',
+  //   id: 'undefined',
+  //   licence: 'community',
+  //   maxProjects: '1'
+  // })
 
-  // In a real app you would probably not redirect every route to `/`
-  // however it is important to check `to.path` before redirecting or you
-  // might get an infinite redirect loop
-  debug('Nuxt Middleware')
-  debug(to.path)
-  if (to.path === '/editor') {
-    const accountService = new AccountServiceProxy(config)
-    const response = await accountService.findForAccessToken(accessToken.value)
-    currentUser.value = response.data.data
-  }
+  // // In a real app you would probably not redirect every route to `/`
+  // // however it is important to check `to.path` before redirecting or you
+  // // might get an infinite redirect loop
+  // debug('Nuxt Middleware')
+  // debug(to.path)
+  // if (to.path === '/editor') {
+  //   const accountService = new AccountServiceProxy(config)
+  //   const response = await accountService.findForAccessToken(accessToken.value)
+  //   currentUser.value = response.data.data
+  // }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -24,6 +24,9 @@
 
   const config = useRuntimeConfig()
 
+  const accessToken = useCookie('access_token')
+  const refreshToken = useCookie('refresh_token')
+
   onMounted(() => {
       initAccordions();
       initCarousels();
@@ -53,13 +56,26 @@
     })
   }
 
+  async function getLoggedUserData(accessToken) {
+    debug('getLoggedUserData')
+    const accountService = new AccountServiceProxy(config)
+    const response = await accountService.findForAccessToken(accessToken)
+    debug(response.data.data )
+    currentUser.value = response.data.data
+  }
+
   async function updateSession(data) {
     debug('SESSION')
     debug(data)
+    accessToken.value = data.accessToken
+    refreshToken.value = data.refreshToken
+    nextTick(async () => {
+      await getLoggedUserData(accessToken.value)
    
-    const router = useRouter()
-    router.push({
-      path: '/editor',
+      const router = useRouter()
+      router.push({
+        path: '/editor',
+      })
     })
   }
 </script>

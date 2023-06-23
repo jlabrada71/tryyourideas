88192f93044f92c40a52a9e51203d238472858ea
jlabrada <jlabrada@yahoo.com>
2023-06-23 16:20:07 -0300

    Adding project components to add component menu

--- components/ItemTypeMenu.vue
+++ components/ItemTypeMenu.vue
@@ -1,27 +1,9 @@
 <template>
-  <div class="absolute top-20 left-72 w-80 h-auto  bg-slate-100 p-5 shadow-md rounded-2xl flex flex-col">
-    <h1 id="id-2-1" class="flex flex-row w-full h-10">
-      Html 5 Tags
-    </h1>
-    <section id="id-2-2" class="flex flex-row flex-wrap bg-slate-100 p-2">
-      <button
-        type="button"
-        v-for="type in typeList" 
-        class="
-          flex flex-row
-          bg-slate-50
-          w-20
-          h-10
-          p-1
-          hover:bg-slate-200
-          active:bg-slate-300
-          focus:bg-slate-300
-          focus:border-2 focus:border-slate-400
-        "
-        @click="selectType(type)"
-        >{{type.name}}
-      </button>
-    </section>
+  <div class="absolute top-20 left-72 w-80 h-auto  bg-slate-200 p-5 shadow-md rounded-2xl flex flex-col">
+
+    <div class="flex flex-row"><ItemTypeTab @selected:category="selectHtml5">Html 5</ItemTypeTab><ItemTypeTab @selected:category="selectProject">Project</ItemTypeTab></div>
+    <ItemTypeHtml5 v-if="html5Tab.active" @select="selectType"></ItemTypeHtml5>
+    <ItemTypeProject  v-if="projectTab.active" :components="props.components" @select="selectType"></ItemTypeProject>
     <footer
       id="id-2-3"
       class="
@@ -30,8 +12,8 @@
         mt-5
         bg-slate-100
         w-full
-        h-10
-        px-5
+
+        p-5
       "
     >
       <button
@@ -77,6 +59,28 @@
 <script setup>
   import { typeList } from '@/lib/typeList.js'
   const emit = defineEmits(['cancel', 'selected'])
+  const props = defineProps({
+    components: {
+      type: Object
+    }
+  })
+
+  onMounted(() => {
+    console.log('Item Type Menu')
+    props.components.forEach(component => {
+      console.log(component.name)
+    })
+  })
+
+  const html5Tab = ref({
+    active: true
+  })
+
+  const projectTab = ref({
+    active: false
+  })
+
+  const tabs = [html5Tab, projectTab]
 
   const selectedType = ref(null)
 
@@ -91,6 +95,22 @@
   function cancelSelectType() {
       emit('cancel')
       clearSelection()
+  }
+
+  function detactivateAll() {
+    tabs.forEach(tab => tab.value.active = false)
+  }
+
+  function selectHtml5() {
+    detactivateAll()
+    html5Tab.value.active = true
+
+  }
+
+  function selectProject() {
+    detactivateAll()
+    projectTab.value.active = true
+
   }
 
   function acceptSelectType() {
---
+++ components/ItemTypeProject.vue
@@ -0,0 +1,42 @@
+<template>
+  <section id="id-2-2" class="flex flex-row flex-wrap bg-slate-100 p-2">
+    <button
+      type="button"
+      v-for="component in props.components" 
+      class="
+        flex flex-row
+        bg-slate-50
+        w-full
+        h-8
+        p-1
+        hover:bg-slate-200
+        active:bg-slate-300
+        focus:bg-slate-300
+        focus:border-2 focus:border-slate-400"
+      @click="selectType(component)"
+      >{{component.name}}
+    </button>
+  </section>
+</template>
+
+<script setup>
+  const emit = defineEmits(['selected'])
+
+  const props = defineProps({
+    components: {
+      type: Array
+    }
+  })
+
+  onMounted(() => {
+    console.log('Item Type Project')
+    props.components.forEach(component => {
+      console.log(component.name)
+    })
+  })
+
+  function selectType(component) {
+    emit('selected', component)
+  }
+
+</script>

acd512e4ae6396b635b7b15e3444fddc9f34162b
jlabrada <jlabrada@yahoo.com>
2023-06-22 22:44:51 -0300

    Fixing gradient issues

--- components/selectors/GradientDirection.vue
+++ components/selectors/GradientDirection.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <InputListWithTag :options="directionList" :option="direction" @update:option="value=>emit('update:direction', value)">
-      <h1>Direction: {{direction}}</h1>
+    <InputListWithTag :options="directionList" :option="direction" tag="bg-gradient" @update:option="value=>emit('update:direction', value)">
+      <h1>Direction: </h1>
     </InputListWithTag>
   </div>
 </template>
@@ -9,7 +9,7 @@
   const props = defineProps(['direction'])
   const emit = defineEmits(['update:direction'])
 
- const directionList = ['unset', 'bg-none', 'bg-gradient-to-t', 'bg-gradient-to-tr', 'bg-gradient-to-r', 'bg-gradient-to-br', 'bg-gradient-to-b', 'bg-gradient-to-bl', 'bg-gradient-to-l', 'bg-gradient-to-tl']
+ const directionList = ['unset', 'to-t', 'to-tr', 'to-r', 'to-br', 'to-b', 'to-bl', 'to-l', 'to-tl']
 
 </script>
 

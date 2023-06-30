01ab7d725dd987b5dab5f86cc33569ac663ff6e0
jlabrada <jlabrada@yahoo.com>
2023-06-30 19:27:48 -0300

    BUGFIX: Sometimes the project can not be loaded

--- pages/config.vue
+++ pages/config.vue
@@ -7,6 +7,7 @@
     Project: <br>
     Name: {{project.name}} <br>
     User: <br>
+    Id: {{ currentUser.id}} <br> <br>
     Name: {{ currentUser.name}} <br> <br>
     accessToken: {{ accessToken }} <br> <br>
     <div class="p-2 flex gap-2">
@@ -39,10 +40,20 @@
   const accessToken = useCookie('access_token')
 
   function cleanProject() {
-    project.value = null
+    project.value = {
+      name: 'Default',
+      dirty: false,
+      components: [],
+    }
   }
 
   function cleanUser() {
-    currentUser.value = null
+    currentUser.value = {
+      name: 'anonimous',
+      email: 'undefined',
+      id: 'undefined',
+      licence: 'community',
+      maxProjects: '1'
+    }
   }
 </script>

a0420c4f7a694631d2fd97839b757cf829f18637
jlabrada <jlabrada@yahoo.com>
2023-07-03 15:49:10 -0300

    Adding application footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,3 @@
 <template>
-  <div class="flex flex-col min-h-screen">
-    <div class="h-full">
-      <slot />
-    </div>
-    <footer>
-      <div class="h-20 w-screen p-5 bg-black text-white">
-        <span class=" h-full w-full ">&copy; {{ new Date().getFullYear() }} {{config.public.env}} <ProductVersion></ProductVersion></span>
-      </div>
-    </footer>
-  </div>
+  <slot />
 </template>
-<script setup>
-  const config = useRuntimeConfig()
-</script>

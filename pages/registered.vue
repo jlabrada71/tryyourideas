0526e773749337fe9681535564254a6b5c7209e1
jlabrada <jlabrada@yahoo.com>
2023-07-03 22:06:58 -0300

    registered design added

--- pages/registered.vue
+++ pages/registered.vue
@@ -1,4 +1,17 @@
 <template>
-  Your account have been registered.
-  To activate your account click on the link of the activation email that was sent to your email account.
+  <HomeNavigationBar/>
+  <div class="flex w-full h-screen flex-col">
+    <AccountActivation :message="message" :result="result"/>
+  </div>  
 </template>
+<script setup>
+
+const result = ref('')
+const message = ref('')
+
+onMounted(async () => {
+  result.value = 'Account have been registered'
+  message.value = 'To activate your account click on the link of the activation email that was sent to your email account.'
+})
+
+</script>

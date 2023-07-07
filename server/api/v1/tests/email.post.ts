93c58e679a6384b2527efc5edf4a8f230e3a05d5
jlabrada <jlabrada@yahoo.com>
2023-07-07 18:35:32 -0300

    Changing the emails account

--- server/api/v1/tests/email.post.ts
+++ server/api/v1/tests/email.post.ts
@@ -1,6 +1,6 @@
 // curl -H "Content-Type: application/x-www-form-urlencoded" -d "param1=value1&param2=value2"  -X POST 'localhost:3000/api/test' 
 //  for file: -d @data.txt
-// curl -H "Content-Type: application/json" -d '{"title": "generation", "email":"jlabrada@yahoo.com", "content":"Download Link"}' -X POST 'localhost:3000/api/v1/tests/email'
+// curl -H "Content-Type: application/json" -d '{"title": "test email", "email":"jlabrada@yahoo.com", "content":"This is a test email"}' -X POST 'localhost:3000/api/v1/tests/email'
 //  for file: -d @data.json
 
 

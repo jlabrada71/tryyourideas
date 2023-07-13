a57d2451e81117ab158b993c7c8fef5d3ad8571c
jlabrada <jlabrada@yahoo.com>
2023-07-13 20:40:09 -0300

    Adding frame to selected component

---
+++ pages/tests/selector-frame.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="bg-slate-100 flex justify-center items-center w-96 h-96 m-40 relative ">
+    <div class="w-40 h-40 relative">
+      <div class="absolute w-full h-full bg-transparent border-[2px] border-blue-800 z-10">
+      </div>
+      <div class="absolute left-0 top-0 w-2 h-2 bg-transparent rounded-full border-2 border-blue-800 z-10"></div>
+      <div class="absolute left-0 bottom-0 w-2 h-2 bg-transparent rounded-full border-2 border-blue-800 z-10"></div>
+      <div class="absolute right-0 top-0 w-2 h-2 bg-transparent rounded-full border-2 border-blue-800 z-10"></div>
+      <div class="absolute right-0 bottom-0 w-2 h-2 bg-transparent rounded-full border-2 border-blue-800 z-10"></div>
+      <div class="absolute  w-40 h-40 bg-teal-300 flex justify-center items-center  border-8 border-teal-500">
+        hello
+      </div>
+    </div>
+  </div>
+  <div class="bg-slate-100 flex justify-center items-center w-fit h-fit m-40 p-20 ">
+    <div class="bg-clip-border p-6 bg-violet-600 border-4 border-violet-300 border-dashed">text</div>
+    <div class="bg-clip-padding p-6 bg-violet-600 border-4 border-violet-300 border-dashed">text</div>
+    <div class="bg-clip-content p-6 bg-violet-600 border-4 border-violet-300 border-dashed">text</div>
+    <div class="bg-clip-text p-6 bg-violet-600 border-4 border-violet-300 border-dashed">text</div>
+  </div>
+  <div class="bg-slate-100 flex justify-center items-center w-fit h-fit m-40 p-20 relative">
+      <div class="absolute left-0 z-50 w-14 rounded-full h-14 bg-blue-300 border-2 border-blue-400 flex justify-center items-center">05</div>
+      <div class="absolute left-10 z-40 w-14 rounded-full h-14  bg-blue-300 border-2 border-blue-400 flex justify-center items-center">04</div>
+      <div class="absolute left-20 z-30 w-14 rounded-full h-14  bg-blue-300 border-2 border-blue-400 flex justify-center items-center">03</div>
+      <div class="relative left-10 z-20 w-14 rounded-full h-14  bg-blue-300 border-2 border-blue-400 flex justify-center items-center">02</div>
+      <div class="relative left-5 z-10 w-14 rounded-full h-14   bg-blue-300 border-2 border-blue-400 flex justify-center items-center">01</div>
+  </div>
+</template>

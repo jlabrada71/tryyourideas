33d6fb0106343a0cfc7e499bcba1e6165e78558b
jlabrada <jlabrada@yahoo.com>
2023-07-15 20:08:16 -0300

    Adding email to system status

--- components/SystemStatus.vue
+++ components/SystemStatus.vue
@@ -24,6 +24,14 @@
         {{name}}
       </span>
     </div>
+    <div id="id-15-3" class="bg-slate-200 px-2 flex-row flex ">
+      <span id="id-15-3-1" class="bg-slate-200 flex-row flex ">
+        Email:
+      </span>
+      <span id="id-15-3-2" class="bg-slate-200 px-2 flex-row font-bold flex ">
+        {{email}}
+      </span>
+    </div>
     <button id="id-15-4" type="button" class="bg-slate-200 w-auto h-10 mx-4 py-2 px-4 self-end rounded-full flex-row flex hover:bg-slate-300 hover:w-auto hover:h-10 hover:py-2 hover:px-4 hover:rounded-full hover:flex-row hover:flex ">
       Logout
     </button>
@@ -38,6 +46,9 @@ const props = defineProps({
         type: String
     },
     project: {
+        type: String
+    },
+    email: {
         type: String
     }
 

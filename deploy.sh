e36753efc7cf8446c1c54d049ff7d90fc1701c4e
jlabrada <jlabrada@yahoo.com>
2023-07-17 21:47:34 -0300

    Improving deploy

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,29 @@
 GIT_HASH=$(git rev-parse --short HEAD)
 GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 GIT_TAG=$(git describe --tags)
-echo "<template>Version $GIT_TAG ($GIT_BRANCH)</template>" > components/ProductVersion.vue
-npm run build
-tar -czf tryyourideas.com.tar .output
-cd ../aws-config
-./copy_ssh.sh ../tryyourideascom tryyourideas.com.tar
-cd ../tryyourideascom
+GIT_TAG_ABBR=$(git describe --tags --abbrev=0)
+NEW_TAG=$1
+NEW_TAG_MSG=$2
+
+RELEASE_NOTES=$(git log $GIT_TAG_ABBR..HEAD --oneline)
+
+if [ -z "$NEW_TAG" ]
+then 
+  echo "Last tag: $GIT_TAG"
+  echo $RELEASE_NOTES
+
+  echo "usage: ./deploy.sh <new tag> '<new tag message>'"
+else
+  create_tag $NEW_TAG "$NEW_TAG_MSG"
+  echo '=============' >> RELEASE_NOTES.txt
+  echo "$NEW_TAG => $NEW_TAG_MSG" >> RELEASE_NOTES.txt
+  echo '---------' >> RELEASE_NOTES.txt
+  echo $RELEASE_NOTES >> RELEASE_NOTES.txt
+
+  echo "<template>Version $NEW_TAG ($GIT_BRANCH)</template>" > components/ProductVersion.vue
+  npm run build
+  tar -czf tryyourideas.com.tar .output
+  cd ../aws-config
+  ./copy_ssh.sh ../tryyourideascom tryyourideas.com.tar
+  cd ../tryyourideascom
+fi

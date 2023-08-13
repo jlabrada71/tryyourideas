20ce02face2201a5cf831581249a5deab57ac1d8
jlabrada <jlabrada@yahoo.com>
2023-08-12 23:12:17 -0300

    Adding tutorials to home page

---
+++ components/Home/Tutorials.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="bg-slate-100 rounded-xl p-4">
+    <h1 class="font-bold text-3xl">Tutorials</h1>
+    <div class="bg-slate-200 w-full h-64 p-5 flex-row flex-wrap flex gap-4 rounded-xl">
+      <HomeTutorialItem 
+        title="How to create simple gradients" 
+        url="https://www.youtube.com/watch?v=4ZQ8p-kZQA8&t=81s"
+        image="/images/gradient-tutorial.png">
+      </HomeTutorialItem>
+      <HomeTutorialItem 
+        title="How to create a responsive component" 
+        url="https://www.youtube.com/watch?v=Q7K64NZioIY"
+        image="/images/responsive-tutorial.png">
+      </HomeTutorialItem>
+      <HomeTutorialItem 
+        title="How to bind properties (coming soon)" 
+        url=""
+        image="logo.png">
+      </HomeTutorialItem>
+      <HomeTutorialItem 
+        title="How to create gradient text (coming soon)" 
+        url=""
+        image="logo.png">
+      </HomeTutorialItem>
+    </div>
+  </div>
+</template>
+<script setup>
+const props = defineProps({})
+const emit = defineEmits([])
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -9,16 +9,16 @@
   </div>
 
   <div class="flex justify-center w-full">
-    <div class="p-10 gap-10 m-10 w-4/5 flex flex-col content-center">
-      <div class="flex flex-col gap-10 p-10 m-10 ">
-        <span class="text-5xl flex w-full justify-center">The easiest way to create VueJS components with TailwindCSS</span>
+    <div class="p-10 gap-10 w-4/5 flex flex-col content-center">
+      <div class="flex flex-col gap-10 p-10 ">
+        <span class="text-3xl flex w-full justify-center">The easiest way to create VueJS components with TailwindCSS</span>
         <div class="flex flex-wrap w-full justify-around">
           <img class="w-1/3" src="/assets/images/vue-js.png" alt="VueJS logo">
           <img class="w-1/3" src="/assets/images/tailwind-css.png" alt="TailwindCSS logo">
         </div>
-        
       </div>
-      <div class="flex justify-around gap-10 p-10 m-10 bg-slate-600 rounded-xl">
+      <HomeTutorials></HomeTutorials>
+      <div class="flex justify-around gap-10 p-10 m-8 bg-slate-600 rounded-xl">
         <img class="w-1/2" src="/assets/images/create-visually.png" alt="Create your components Visually">
         <div class="w-1/2">
           <p class="text-5xl text-slate-50 font-bold">Create your components in the way you wanted</p>
@@ -38,26 +38,26 @@
       <div class="flex justify-around gap-10 p-10 m-10  bg-slate-100 rounded-xl">
         <div class="w-1/4 bg-slate-300 rounded-xl">
           <img  class="h-1/4 w-full rounded-t-xl" src="/assets/images/gradient.png" alt="Gradient Image">
-          <p class="text-4xl mx-10 my-5 font-bold">Gradients</p>
-          <p class="text-2xl m-10">Finding the right color combination and directions for gradients is quite time consuming. With our component editor this can be done in two mouse clicks.</p>
+          <p class="text-3xl mx-10 my-5 font-bold">Gradients</p>
+          <p class="text-xl m-10">Finding the right color combination and directions for gradients is quite time consuming. With our component editor this can be done in two mouse clicks.</p>
           <p class="text-2xl m-10"></p>
         </div>
         <div class="w-1/4 bg-slate-300 rounded-xl">
           <img  class="h-1/4 w-full rounded-t-xl"  src="/assets/images/animations.png" alt="Animations image">
-          <p class="text-4xl mx-10 my-5 font-bold">Animations</p>
-          <p class="text-2xl m-10">When you are giving life to your components, your expectation is to see how this is accomplished and quickly perfect the animation. That is what we allow with our tool. Shadows, gradients, placements and many other animations.</p>
+          <p class="text-3xl mx-10 my-5 font-bold">Animations</p>
+          <p class="text-xl m-10">When you are giving life to your components, your expectation is to see how this is accomplished and quickly perfect the animation. That is what we allow with our tool. Shadows, gradients, placements and many other animations.</p>
           <p class="text-2xl m-10"></p>
         </div>
         <div class="w-1/4 bg-slate-300 rounded-xl">
           <img class="h-1/4 w-full rounded-t-xl" src="/assets/images/composition.png" alt="Composition image">
-          <p class="text-4xl mx-10 my-5 font-bold">Composition</p>
-          <p class="text-2xl m-10">There are out there many tools that offer individual components that you need to struggle with to incorporate to your design. We allow component composition in a natural way. With no hassle.</p>
+          <p class="text-3xl mx-10 my-5 font-bold">Composition</p>
+          <p class="text-xl m-10">There are out there many tools that offer individual components that you need to struggle with to incorporate to your design. We allow component composition in a natural way. With no hassle.</p>
           <p class="text-2xl m-10"></p>
         </div>
         <div class="w-1/4 bg-slate-300 rounded-xl">
           <img class="h-1/4 w-full rounded-t-xl" src="/assets/images/properties.png" alt="Properties image">
-          <p class="text-4xl mx-10 my-5 font-bold">Properties</p>
-          <p class="text-2xl m-10">Creating properties, variables and events and orchestrating components is both, time-consuming and error prone. It's also difficult to remember which events are triggered by each component. We take care of that.</p>
+          <p class="text-3xl mx-10 my-5 font-bold">Properties</p>
+          <p class="text-xl m-10">Creating properties, variables and events and orchestrating components is both, time-consuming and error prone. It's also difficult to remember which events are triggered by each component. We take care of that.</p>
           <p class="text-2xl m-10"></p>
         </div>
         

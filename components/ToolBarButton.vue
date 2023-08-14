a57114bac0f2e797fabf3ca322a3eacc99ef5328
jlabrada <jlabrada@yahoo.com>
2023-08-14 14:40:04 -0300

    BUGFIX: Remove the selected state for pasted item BUGFIX: Toolbar buttons UX Improved

--- components/ToolBarButton.vue
+++ components/ToolBarButton.vue
@@ -2,7 +2,7 @@
   <button id="id-8-1" type="button"
     :data-modal-target="props.target"
     :data-modal-toggle="props.target"
-    class=" flex flex-row justify-center items-center bg-slate-200 w-10 h-10 hover:flex hover:flex-row hover:justify-center hover:items-center hover:bg-slate-300 hover:w-10 hover:h-10 ">
+    class=" flex flex-row justify-center items-center bg-slate-200 w-10 h-10  hover:bg-slate-300 active:bg-slate-400 ">
     <slot></slot>
   </button>
 </template>
